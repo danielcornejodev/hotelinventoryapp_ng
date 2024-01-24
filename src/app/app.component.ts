1d03a80e3f4c542c9e5094551a85052490359239
Daniel Cornejo <contact@danielcornejo.dev>
2024-01-24 08:45:21 -0500

    3:21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RoomsComponent],
+  templateUrl: './app.component.html', //alternatively you could use an inline HTML template here using backticks.
   styleUrl: './app.component.scss'
+  //styles: [`h1 { color: red}`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
